41d94d4cf4befcc740a2d8f7ec2692ccdedeb06f
Burwor <38257629+Burwor@users.noreply.github.com>
2019-12-17 08:58:27 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -31,6 +31,13 @@ void initialize() {
 	pros::lcd::initialize();
 	pros::lcd::set_text(1, "meegle mech robot brain");
 	pros::lcd::register_btn1_cb(on_center_button);
+	pros::Controller master (pros::E_CONTROLLER_MASTER);
+	pros::Motor left_mtr(LEFT_MTR_PORT);
+	pros::Motor right_mtr (RIGHT_MTR_PORT, true);
+	pros::Motor stack_mtr(STACK_MTR_PORT);
+	pros::Motor intake_mtr(INTAKE_MTR_PORT);
+	pros::Vision vision_sensor(VISION_PORT);
+	stack_mtr.set_gearing(pros::E_MOTOR_GEARSET_36);
 }
 
 
@@ -59,42 +66,58 @@ void disabled() {}
 void competition_initialize() {}
 
 
-void autonomous() {}
+void autonomous() {
+	while(true)
+	{
+		pros::vision_object cube = vision_sensor.get_by_size(0);
+		power = 0;
+		turn = 0;
+		if(!(cube.x_middle_coord<180 && cube.x_middle_coord>130) && cube.x_middle_coord > 0) // if there is not an object and if there is, not in the middle
+		{
+			turn = (cube.x_middle_coord-155) / 5;
+			unsigned int present = 0;
+			pros::lcd::print(6, "imma be turnin'");
+		}
+		else pros::lcd::print(6,"aint turnin for now");
+		if((cube.height >= 30 || cube.width >=30) && (cube.height <= 200 || cube.width <=200) && cube.x_middle_coord > 0)
+		{
+			power = 127; // if there is a cube(cube.x_middle_coord > 0) and if it is in the appropriate size, RUN INTO IT!!!!
+			if(present != 0) unsigned int present = pros::millis();
+		}
+		if(pros::millis() == )
+		left_mtr.move(power+turn);
+		right_mtr.move(power-turn);
+		pros::delay(5);
+	}
+}
 
 
 void opcontrol() {
-	pros::Controller master (pros::E_CONTROLLER_MASTER);
-	pros::Motor left_mtr(LEFT_MTR_PORT);
-	pros::Motor right_mtr (RIGHT_MTR_PORT, true);
-	pros::Motor stack_mtr(STACK_MTR_PORT);
-	pros::Motor intake_mtr(INTAKE_MTR_PORT);
-	pros::Vision vision_sensor(VISION_PORT);
-	stack_mtr.set_gearing(pros::E_MOTOR_GEARSET_36);
 	while (true) {
 		int power = master.get_analog(ANALOG_LEFT_Y);
 		int turn = master.get_analog(ANALOG_LEFT_X);
 		//vision sensor start
-		pros::vision_object cube = vision_sensor.get_by_sig(0, 1); // yesil dude
+		pros::vision_object cube = vision_sensor.get_by_size(0); // yesil dude
 		pros::lcd::print(5, "%d", cube.x_middle_coord);
 
 		//a function which makes the robot direct to the cube it sees
 		while(master.get_digital(DIGITAL_X))
 		{
-				pros::vision_object cube = vision_sensor.get_by_sig(0,1);
+				pros::vision_object cube = vision_sensor.get_by_size(0);
 				power = 0;
 				turn = 0;
 			if(!(cube.x_middle_coord<180 && cube.x_middle_coord>130) && cube.x_middle_coord > 0) // if there is not an object and if there is, not in the middle
 			{
-				turn = (cube.x_middle_coord-155) / 7;
+				turn = (cube.x_middle_coord-155) / 5;
 				pros::lcd::print(6, "imma be turnin'"); //3.5 for competition
 			}
 			else pros::lcd::print(6,"aint turnin for now");
-			if((cube.height >= 30 || cube.width >=30) && (cube.height <= 200 || cube.width <=200) && cube.x_middle_coord > 0) power = 40; // if there is a cube(cube.x_middle_coord > 0) and if it is in the appropriate size, RUN INTO IT!!!!
+			if((cube.height >= 30 || cube.width >=30) && (cube.height <= 200 || cube.width <=200) && cube.x_middle_coord > 0) power = 127; // if there is a cube(cube.x_middle_coord > 0) and if it is in the appropriate size, RUN INTO IT!!!!
 			left_mtr.move(power+turn);
 			right_mtr.move(power-turn);
 			pros::delay(5);
 		}
-		
+
 		while(master.get_digital(DIGITAL_L1))
 	  {
 			stack_mtr.move(STACK_MTR_POWER);
